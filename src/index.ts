cad22666f9da2e9618c4c72f78feb8b458bc46c0
Jun Guo <gjuoun@gmail.com>
2024-06-24 02:23:38 -0400

    chore: Update package.json start script for playground
    ```

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Inject, Injectable } from "./decorators.js";
-export { Container } from "./container.js";
+export { Container, Depends } from "./container.js";
 export { Module, type ModuleOptions } from "./module.js";
 export { DynamicModule, type DynamicModuleOptions } from "./dynamicModule.js";
 export { createRouter, type Router } from "./router.js";
